7589f682dadffefe6828381f285e06678703dc8a
Kenneth Traynor <cyannotive@gmail.com>
2021-04-12 19:44:52 -0400

    feat: set up webpack bundling and building

--- src/scripts/utils/constants.js
+++ src/scripts/utils/constants.js
@@ -33,3 +33,22 @@ export const defaultConfig = {
   inputErrorClass: "popup__input_type_error",
   errorClass: "popup__error_visible"
 };
+
+
+// Modals
+const profileModal = document.querySelector('.popup_type_profile');
+const newCardModal = document.querySelector('.popup_type_new-card');
+
+// Forms
+export const profileForm = profileModal.querySelector('.popup__form');
+export const newCardForm = newCardModal.querySelector('.popup__form');
+export const profileFormName = profileForm.querySelector('.popup__input_type_name');
+export const profileFormAbout = profileForm.querySelector('.popup__input_type_about');
+
+// Profile Values
+export const profileName = document.querySelector('.profile__name').textContent;
+export const profileAbout =  document.querySelector('.profile__about').textContent;
+export const profileEditButton = document.querySelector('.profile__edit-button');
+
+// Add Card Button
+export const addCardButton = document.querySelector('.profile__add-button');
